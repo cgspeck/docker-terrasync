017550912d9515c60ad53d52c4ad39d25335dec2
Chris Speck <cgspeck@gmail.com>
2019-04-07 09:40:42 +1000

    Add some print statements to show length of run and sync mode

--- wrapper.py
+++ wrapper.py
@@ -1,5 +1,6 @@
+#! /usr/bin/env python3
 import os
-import sys
+from datetime import datetime
 from pathlib import Path
 
 from vendor.terrasync import main
@@ -10,6 +11,13 @@ def runner():
     fp = Path(os.environ['TARGET_DIR'], ".full-sync-complete")
     args.quick = fp.exists()
 
+    if args.quick:
+        print("A previously completed full sync exists, doing a quick sync", flush=True)
+
+    dt_start = datetime.now()
+
+    print(f"Starting run at {dt_start}", flush=True)
+
     terraSync = main.TerraSync(args.mode, args.report, args.url, args.target,
                           args.quick, args.removeOrphan,
                           main.DownloadBoundaries(args.top, args.left, args.bottom,
@@ -18,8 +26,13 @@ def runner():
 
     if args.report:
         report.printReport()
+    from time import sleep; sleep(10)
 
     fp.touch()
+    dt_end = datetime.now()
+    elapsed = dt_end - dt_start
+    print(f"Sync complete at {dt_end}, elapsed: {elapsed}")
+    print(f"Sync took {elapsed.days} days, {elapsed.seconds // 3600} hours, {elapsed.seconds // 60} minutes and {elapsed.seconds} seconds")
 
 if __name__ == '__main__':
     runner()
